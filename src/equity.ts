a04c7fad1ba8745398d579a0b1e4c09d0e9fc9ec
samclassix <96245183+samclassix@users.noreply.github.com>
2024-05-20 15:01:41 +0200

    Extending Position Schema and Indexing, Excluding Bug in Equity (#1)
    * adds "original" property to position indexed
    
    * QUICK FIX: ponder gets stuck, rn file to exclude from ponder
    
    * extending position properties, storing const data from position, update original
    
    * added erc20 names
    
    * readding equity, bug excluded
    
    * rm file and update prettier

--- src/equity.ts
+++ src/equity.ts
@@ -1,176 +1,168 @@
-import { Context, ponder } from "@/generated";
-import { zeroAddress } from "viem";
+import { Context, ponder } from '@/generated';
+import { zeroAddress } from 'viem';
 
-ponder.on("Equity:Trade", async ({ event, context }) => {
-  const { Trade, VotingPower, TradeChart, ActiveUser } = context.db;
+ponder.on('Equity:Trade', async ({ event, context }) => {
+	const { Trade, VotingPower, TradeChart, ActiveUser } = context.db;
 
-  await Trade.create({
-    id: event.args.who + "_" + event.block.timestamp.toString(),
-    data: {
-      trader: event.args.who,
-      amount: event.args.totPrice,
-      shares: event.args.amount,
-      price: event.args.newprice,
-      time: event.block.timestamp,
-    },
-  });
+	await Trade.create({
+		id: event.args.who + '_' + event.block.timestamp.toString(),
+		data: {
+			trader: event.args.who,
+			amount: event.args.totPrice,
+			shares: event.args.amount,
+			price: event.args.newprice,
+			time: event.block.timestamp,
+		},
+	});
 
-  await VotingPower.upsert({
-    id: event.args.who,
-    create: {
-      address: event.args.who,
-      votingPower: event.args.amount,
-    },
-    update: ({ current }) => ({
-      votingPower: current.votingPower + event.args.amount,
-    }),
-  });
+	await VotingPower.upsert({
+		id: event.args.who,
+		create: {
+			address: event.args.who,
+			votingPower: event.args.amount,
+		},
+		update: ({ current }) => ({
+			votingPower: current.votingPower + event.args.amount,
+		}),
+	});
 
-  const startTime = (event.block.timestamp / 86400n) * 86400n;
-  await TradeChart.upsert({
-    id: startTime.toString(),
-    create: {
-      time: startTime,
-      lastPrice: event.args.newprice,
-    },
-    update: ({ current }) => ({
-      lastPrice: event.args.newprice,
-    }),
-  });
+	const startTime = (event.block.timestamp / 86400n) * 86400n;
+	await TradeChart.upsert({
+		id: startTime.toString(),
+		create: {
+			time: startTime,
+			lastPrice: event.args.newprice,
+		},
+		update: ({ current }) => ({
+			lastPrice: event.args.newprice,
+		}),
+	});
 
-  await ActiveUser.upsert({
-    id: event.args.who,
-    create: {
-      lastActiveTime: event.block.timestamp,
-    },
-    update: () => ({
-      lastActiveTime: event.block.timestamp,
-    }),
-  });
+	await ActiveUser.upsert({
+		id: event.args.who,
+		create: {
+			lastActiveTime: event.block.timestamp,
+		},
+		update: () => ({
+			lastActiveTime: event.block.timestamp,
+		}),
+	});
 });
 
-ponder.on("Equity:Transfer", async ({ event, context }) => {
-  const { VotingPower, ActiveUser } = context.db;
+ponder.on('Equity:Transfer', async ({ event, context }) => {
+	const { VotingPower, ActiveUser } = context.db;
 
-  if (event.args.from == zeroAddress || event.args.to == zeroAddress) return;
+	if (event.args.from == zeroAddress || event.args.to == zeroAddress) return;
 
-  await VotingPower.update({
-    id: event.args.from,
-    data: ({ current }) => ({
-      votingPower: current.votingPower - event.args.value,
-    }),
-  });
+	await VotingPower.update({
+		id: event.args.from,
+		data: ({ current }) => ({
+			votingPower: current.votingPower - event.args.value,
+		}),
+	});
 
-  await VotingPower.upsert({
-    id: event.args.to,
-    create: {
-      address: event.args.to,
-      votingPower: event.args.value,
-    },
-    update: ({ current }) => ({
-      votingPower: current.votingPower + event.args.value,
-    }),
-  });
+	await VotingPower.upsert({
+		id: event.args.to,
+		create: {
+			address: event.args.to,
+			votingPower: event.args.value,
+		},
+		update: ({ current }) => ({
+			votingPower: current.votingPower + event.args.value,
+		}),
+	});
 
-  await ActiveUser.upsert({
-    id: event.args.from,
-    create: {
-      lastActiveTime: event.block.timestamp,
-    },
-    update: () => ({
-      lastActiveTime: event.block.timestamp,
-    }),
-  });
-  await ActiveUser.upsert({
-    id: event.args.to,
-    create: {
-      lastActiveTime: event.block.timestamp,
-    },
-    update: () => ({
-      lastActiveTime: event.block.timestamp,
-    }),
-  });
+	await ActiveUser.upsert({
+		id: event.args.from,
+		create: {
+			lastActiveTime: event.block.timestamp,
+		},
+		update: () => ({
+			lastActiveTime: event.block.timestamp,
+		}),
+	});
+	await ActiveUser.upsert({
+		id: event.args.to,
+		create: {
+			lastActiveTime: event.block.timestamp,
+		},
+		update: () => ({
+			lastActiveTime: event.block.timestamp,
+		}),
+	});
 });
 
-ponder.on("Equity:Delegation", async ({ event, context }) => {
-  const addParentDelegation = async (
-    delegatedFrom: string | undefined,
-    arrayToAdd: string[]
-  ) => {
-    if (!delegatedFrom) return;
+// FIXME: Ponder gets stuck, no error, process gets stuck, somewhere here...
+// ponder.on('Equity:Delegation', async ({ event, context }) => {
+// 	const addParentDelegation = async (delegatedFrom: string | undefined, arrayToAdd: string[]) => {
+// 		if (!delegatedFrom) return;
 
-    const parentDelegation = await Delegation.upsert({
-      id: delegatedFrom,
-      create: {
-        owner: delegatedFrom,
-        pureDelegatedFrom: arrayToAdd,
-      },
-      update: ({ current }) => ({
-        pureDelegatedFrom: current.pureDelegatedFrom.concat(
-          arrayToAdd.filter(
-            (item) => current.pureDelegatedFrom.indexOf(item) < 0
-          )
-        ),
-      }),
-    });
+// 		const parentDelegation = await Delegation.upsert({
+// 			id: delegatedFrom,
+// 			create: {
+// 				owner: delegatedFrom,
+// 				pureDelegatedFrom: arrayToAdd,
+// 			},
+// 			update: ({ current }) => ({
+// 				pureDelegatedFrom: current.pureDelegatedFrom.concat(
+// 					arrayToAdd.filter((item) => current.pureDelegatedFrom.indexOf(item) < 0)
+// 				),
+// 			}),
+// 		});
 
-    addParentDelegation(parentDelegation.delegatedTo, arrayToAdd);
-  };
+// 		addParentDelegation(parentDelegation.delegatedTo, arrayToAdd);
+// 	};
 
-  const removeParentDelegation = async (
-    delegationTo: string | undefined,
-    arrayToRemove: string[]
-  ) => {
-    if (!delegationTo) return;
+// 	const removeParentDelegation = async (delegationTo: string | undefined, arrayToRemove: string[]) => {
+// 		if (!delegationTo) return;
 
-    const parentDelegation = await Delegation.findUnique({ id: delegationTo });
-    if (parentDelegation) {
-      const pureDelegatedFrom: string[] = [];
-      for (const delegatedFrom of parentDelegation.pureDelegatedFrom) {
-        if (!arrayToRemove.includes(delegatedFrom))
-          pureDelegatedFrom.push(delegatedFrom);
-      }
+// 		const parentDelegation = await Delegation.findUnique({ id: delegationTo });
+// 		if (parentDelegation) {
+// 			const pureDelegatedFrom: string[] = [];
+// 			for (const delegatedFrom of parentDelegation.pureDelegatedFrom) {
+// 				if (!arrayToRemove.includes(delegatedFrom)) pureDelegatedFrom.push(delegatedFrom);
+// 			}
 
-      await Delegation.update({
-        id: delegationTo,
-        data: {
-          pureDelegatedFrom,
-        },
-      });
+// 			await Delegation.update({
+// 				id: delegationTo,
+// 				data: {
+// 					pureDelegatedFrom,
+// 				},
+// 			});
 
-      removeParentDelegation(parentDelegation.delegatedTo, arrayToRemove);
-    }
-  };
+// 			removeParentDelegation(parentDelegation.delegatedTo, arrayToRemove);
+// 		}
+// 	};
 
-  const { Delegation, ActiveUser } = context.db;
-  const delegation = await Delegation.upsert({
-    id: event.args.from,
-    create: {
-      owner: event.args.from,
-      pureDelegatedFrom: [],
-    },
-    update: {},
-  });
+// 	const { Delegation, ActiveUser } = context.db;
+// 	const delegation = await Delegation.upsert({
+// 		id: event.args.from,
+// 		create: {
+// 			owner: event.args.from,
+// 			pureDelegatedFrom: [],
+// 		},
+// 		update: {},
+// 	});
 
-  const arrayToUpdate = [event.args.from];
-  removeParentDelegation(delegation.delegatedTo, arrayToUpdate);
+// 	const arrayToUpdate = [event.args.from];
+// 	removeParentDelegation(delegation.delegatedTo, arrayToUpdate);
 
-  await Delegation.update({
-    id: event.args.from,
-    data: {
-      delegatedTo: event.args.to,
-    },
-  });
+// 	await Delegation.update({
+// 		id: event.args.from,
+// 		data: {
+// 			delegatedTo: event.args.to,
+// 		},
+// 	});
 
-  addParentDelegation(event.args.to, arrayToUpdate);
+// 	addParentDelegation(event.args.to, arrayToUpdate);
 
-  await ActiveUser.upsert({
-    id: event.args.from,
-    create: {
-      lastActiveTime: event.block.timestamp,
-    },
-    update: () => ({
-      lastActiveTime: event.block.timestamp,
-    }),
-  });
-});
+// 	await ActiveUser.upsert({
+// 		id: event.args.from,
+// 		create: {
+// 			lastActiveTime: event.block.timestamp,
+// 		},
+// 		update: () => ({
+// 			lastActiveTime: event.block.timestamp,
+// 		}),
+// 	});
+// });
--- src/mintingHub.ts
+++ src/mintingHub.ts
@@ -1,170 +1,326 @@
-import { ponder } from "@/generated";
-import { Position as PositionABI } from "../abis/Position";
+import { ponder } from '@/generated';
+import { Position as PositionABI } from '../abis/Position';
+import { ERC20 as ERC20ABI } from '../abis/ERC20';
 
-ponder.on("MintingHub:PositionOpened", async ({ event, context }) => {
-  const { client } = context;
-  const { Position, ActiveUser } = context.db;
+ponder.on('MintingHub:PositionOpened', async ({ event, context }) => {
+	const { client } = context;
+	const { Position, ActiveUser } = context.db;
 
-  const limitForClones = await client.readContract({
-    abi: PositionABI,
-    address: event.args.position,
-    functionName: "limitForClones",
-  });
+	// ------------------------------------------------------------------
+	// FROM EVENT & TRANSACTION
+	const { position, owner, zchf, collateral, price } = event.args;
 
-  if (event.transaction.input.includes("0x5cb47919")) {
-    // Cloning, Update original positions limit
-    const originalPosition = event.transaction.input.slice(34, 74);
+	const created: bigint = event.block.timestamp;
 
-    const originalLimitForClones = await client.readContract({
-      abi: PositionABI,
-      address: `0x${originalPosition}`,
-      functionName: "limitForClones",
-    });
+	const isOriginal: boolean = !event.transaction.input.includes('0x5cb47919');
+	const isClone: boolean = !isOriginal;
+	const closed: boolean = false;
+	const denied: boolean = false;
 
-    await Position.update({
-      id: `0x${originalPosition.toLowerCase()}`,
-      data: {
-        limitForClones: originalLimitForClones,
-      },
-    });
-  }
-  await Position.create({
-    id: event.args.position.toLowerCase(),
-    data: {
-      position: event.args.position,
-      owner: event.args.owner,
-      zchf: event.args.zchf,
-      collateral: event.args.collateral,
-      price: event.args.price,
-      created: event.block.timestamp,
-      limitForClones,
-      denied: false,
-      closed: false,
-    },
-  });
+	const original: `0x${string}` = isOriginal ? event.args.position : (`0x${event.transaction.input.slice(34, 74)}` as `0x${string}`);
 
-  await ActiveUser.upsert({
-    id: event.transaction.from,
-    create: {
-      lastActiveTime: event.block.timestamp,
-    },
-    update: () => ({
-      lastActiveTime: event.block.timestamp,
-    }),
-  });
+	// ------------------------------------------------------------------
+	// CONST
+	const minimumCollateral = await client.readContract({
+		abi: PositionABI,
+		address: event.args.position,
+		functionName: 'minimumCollateral',
+	});
+
+	const annualInterestPPM = await client.readContract({
+		abi: PositionABI,
+		address: event.args.position,
+		functionName: 'annualInterestPPM',
+	});
+
+	const reserveContribution = await client.readContract({
+		abi: PositionABI,
+		address: event.args.position,
+		functionName: 'reserveContribution',
+	});
+
+	const start = await client.readContract({
+		abi: PositionABI,
+		address: event.args.position,
+		functionName: 'start',
+	});
+
+	const expiration = await client.readContract({
+		abi: PositionABI,
+		address: event.args.position,
+		functionName: 'expiration',
+	});
+
+	const challengePeriod = await client.readContract({
+		abi: PositionABI,
+		address: event.args.position,
+		functionName: 'challengePeriod',
+	});
+
+	// ------------------------------------------------------------------
+	// ZCHF ERC20
+	const zchfName = await client.readContract({
+		abi: ERC20ABI,
+		address: zchf,
+		functionName: 'name',
+	});
+
+	const zchfSymbol = await client.readContract({
+		abi: ERC20ABI,
+		address: zchf,
+		functionName: 'symbol',
+	});
+
+	const zchfDecimals = await client.readContract({
+		abi: ERC20ABI,
+		address: zchf,
+		functionName: 'decimals',
+	});
+
+	// ------------------------------------------------------------------
+	// COLLATERAL ERC20
+	const collateralName = await client.readContract({
+		abi: ERC20ABI,
+		address: collateral,
+		functionName: 'name',
+	});
+
+	const collateralSymbol = await client.readContract({
+		abi: ERC20ABI,
+		address: collateral,
+		functionName: 'symbol',
+	});
+
+	const collateralDecimals = await client.readContract({
+		abi: ERC20ABI,
+		address: collateral,
+		functionName: 'decimals',
+	});
+
+	const collateralBalance = await client.readContract({
+		abi: ERC20ABI,
+		address: collateral,
+		functionName: 'balanceOf',
+		args: [event.args.position],
+	});
+
+	// ------------------------------------------------------------------
+	// CHANGEABLE
+	const limitForClones = await client.readContract({
+		abi: PositionABI,
+		address: event.args.position,
+		functionName: 'limit',
+	});
+
+	const availableForClones = await client.readContract({
+		abi: PositionABI,
+		address: event.args.position,
+		functionName: 'limitForClones',
+	});
+
+	const minted = await client.readContract({
+		abi: PositionABI,
+		address: event.args.position,
+		functionName: 'minted',
+	});
+
+	// ------------------------------------------------------------------
+	// CALC VALUES
+	// const priceAdjusted = price / BigInt(10 ** (36 - collateralDecimals));
+	const limitForPosition = (collateralBalance * price) / BigInt(10 ** zchfDecimals);
+	const availableForPosition = limitForPosition - minted;
+
+	// ------------------------------------------------------------------
+	// ------------------------------------------------------------------
+	// ------------------------------------------------------------------
+	// If clone, update original position
+	if (isClone) {
+		const originalLimitForClones = await client.readContract({
+			abi: PositionABI,
+			address: original,
+			functionName: 'limit',
+		});
+
+		const originalAvailableForClones = await client.readContract({
+			abi: PositionABI,
+			address: original,
+			functionName: 'limitForClones',
+		});
+
+		await Position.update({
+			id: original.toLowerCase(),
+			data: {
+				limitForClones: originalLimitForClones,
+				availableForClones: originalAvailableForClones,
+			},
+		});
+	}
+
+	// ------------------------------------------------------------------
+	// ------------------------------------------------------------------
+	// ------------------------------------------------------------------
+	// Create position entry for DB
+	await Position.create({
+		id: event.args.position.toLowerCase(),
+		data: {
+			position,
+			owner,
+			zchf,
+			collateral,
+			price,
+
+			created,
+			isOriginal,
+			isClone,
+			denied,
+			closed,
+			original,
+
+			minimumCollateral,
+			annualInterestPPM,
+			reserveContribution,
+			start,
+			expiration,
+			challengePeriod,
+
+			zchfName,
+			zchfSymbol,
+			zchfDecimals,
+
+			collateralName,
+			collateralSymbol,
+			collateralDecimals,
+			collateralBalance,
+
+			limitForPosition,
+			limitForClones,
+			availableForPosition,
+			availableForClones,
+			minted,
+		},
+	});
+
+	await ActiveUser.upsert({
+		id: event.transaction.from,
+		create: {
+			lastActiveTime: event.block.timestamp,
+		},
+		update: () => ({
+			lastActiveTime: event.block.timestamp,
+		}),
+	});
 });
 
-ponder.on("MintingHub:ChallengeStarted", async ({ event, context }) => {
-  const { client } = context;
-  const { Challenge, ActiveUser } = context.db;
-  const { MintingHub } = context.contracts;
+ponder.on('MintingHub:ChallengeStarted', async ({ event, context }) => {
+	const { client } = context;
+	const { Challenge, ActiveUser } = context.db;
+	const { MintingHub } = context.contracts;
 
-  const challenges = await client.readContract({
-    abi: MintingHub.abi,
-    address: MintingHub.address,
-    functionName: "challenges",
-    args: [event.args.number],
-  });
+	const challenges = await client.readContract({
+		abi: MintingHub.abi,
+		address: MintingHub.address,
+		functionName: 'challenges',
+		args: [event.args.number],
+	});
 
-  const period = await client.readContract({
-    abi: PositionABI,
-    address: event.args.position,
-    functionName: "challengePeriod",
-  });
+	const period = await client.readContract({
+		abi: PositionABI,
+		address: event.args.position,
+		functionName: 'challengePeriod',
+	});
 
-  await Challenge.create({
-    id: getChallengeId(event.args.position, event.args.number),
-    data: {
-      position: event.args.position,
-      number: event.args.number,
-      challenger: event.args.challenger,
-      size: event.args.size,
-      start: challenges[1],
-      duration: period,
-      bid: 0n,
-      acquiredCollateral: 0n,
-      filledSize: 0n,
-      status: "Active",
-    },
-  });
-  await ActiveUser.upsert({
-    id: event.transaction.from,
-    create: {
-      lastActiveTime: event.block.timestamp,
-    },
-    update: () => ({
-      lastActiveTime: event.block.timestamp,
-    }),
-  });
+	await Challenge.create({
+		id: getChallengeId(event.args.position, event.args.number),
+		data: {
+			position: event.args.position,
+			number: event.args.number,
+			challenger: event.args.challenger,
+			size: event.args.size,
+			start: challenges[1],
+			duration: period,
+			bid: 0n,
+			acquiredCollateral: 0n,
+			filledSize: 0n,
+			status: 'Active',
+		},
+	});
+	await ActiveUser.upsert({
+		id: event.transaction.from,
+		create: {
+			lastActiveTime: event.block.timestamp,
+		},
+		update: () => ({
+			lastActiveTime: event.block.timestamp,
+		}),
+	});
 });
 
-ponder.on("MintingHub:ChallengeAverted", async ({ event, context }) => {
-  const { client } = context;
-  const { Challenge, ActiveUser } = context.db;
-  const { MintingHub } = context.contracts;
+ponder.on('MintingHub:ChallengeAverted', async ({ event, context }) => {
+	const { client } = context;
+	const { Challenge, ActiveUser } = context.db;
+	const { MintingHub } = context.contracts;
 
-  const challenges = await client.readContract({
-    abi: MintingHub.abi,
-    address: MintingHub.address,
-    functionName: "challenges",
-    args: [event.args.number],
-  });
+	const challenges = await client.readContract({
+		abi: MintingHub.abi,
+		address: MintingHub.address,
+		functionName: 'challenges',
+		args: [event.args.number],
+	});
 
-  const challengeId = getChallengeId(event.args.position, event.args.number);
+	const challengeId = getChallengeId(event.args.position, event.args.number);
 
-  await Challenge.update({
-    id: challengeId,
-    data: ({ current }) => ({
-      filledSize: current.filledSize + event.args.size,
-      status: challenges[3] === 0n ? "Success" : current.status,
-    }),
-  });
-  await ActiveUser.upsert({
-    id: event.transaction.from,
-    create: {
-      lastActiveTime: event.block.timestamp,
-    },
-    update: () => ({
-      lastActiveTime: event.block.timestamp,
-    }),
-  });
+	await Challenge.update({
+		id: challengeId,
+		data: ({ current }) => ({
+			filledSize: current.filledSize + event.args.size,
+			status: challenges[3] === 0n ? 'Success' : current.status,
+		}),
+	});
+	await ActiveUser.upsert({
+		id: event.transaction.from,
+		create: {
+			lastActiveTime: event.block.timestamp,
+		},
+		update: () => ({
+			lastActiveTime: event.block.timestamp,
+		}),
+	});
 });
 
-ponder.on("MintingHub:ChallengeSucceeded", async ({ event, context }) => {
-  const { client } = context;
-  const { Challenge, ActiveUser } = context.db;
-  const { MintingHub } = context.contracts;
+ponder.on('MintingHub:ChallengeSucceeded', async ({ event, context }) => {
+	const { client } = context;
+	const { Challenge, ActiveUser } = context.db;
+	const { MintingHub } = context.contracts;
 
-  const challenges = await client.readContract({
-    abi: MintingHub.abi,
-    address: MintingHub.address,
-    functionName: "challenges",
-    args: [event.args.number],
-  });
+	const challenges = await client.readContract({
+		abi: MintingHub.abi,
+		address: MintingHub.address,
+		functionName: 'challenges',
+		args: [event.args.number],
+	});
 
-  const challengeId = getChallengeId(event.args.position, event.args.number);
+	const challengeId = getChallengeId(event.args.position, event.args.number);
 
-  await Challenge.update({
-    id: challengeId,
-    data: ({ current }) => ({
-      bid: current.bid + event.args.bid,
-      acquiredCollateral:
-        current.acquiredCollateral + event.args.acquiredCollateral,
-      filledSize: current.filledSize + event.args.challengeSize,
-      status: challenges[3] === 0n ? "Success" : current.status,
-    }),
-  });
-  await ActiveUser.upsert({
-    id: event.transaction.from,
-    create: {
-      lastActiveTime: event.block.timestamp,
-    },
-    update: () => ({
-      lastActiveTime: event.block.timestamp,
-    }),
-  });
+	await Challenge.update({
+		id: challengeId,
+		data: ({ current }) => ({
+			bid: current.bid + event.args.bid,
+			acquiredCollateral: current.acquiredCollateral + event.args.acquiredCollateral,
+			filledSize: current.filledSize + event.args.challengeSize,
+			status: challenges[3] === 0n ? 'Success' : current.status,
+		}),
+	});
+	await ActiveUser.upsert({
+		id: event.transaction.from,
+		create: {
+			lastActiveTime: event.block.timestamp,
+		},
+		update: () => ({
+			lastActiveTime: event.block.timestamp,
+		}),
+	});
 });
 
 const getChallengeId = (position: string, number: bigint) => {
-  return `${position}-challenge-${number}`;
+	return `${position}-challenge-${number}`;
 };
